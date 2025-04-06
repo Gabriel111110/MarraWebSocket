80bbb11847da5fb40c7cb879bf9419734f5fd975
Gibbo Marra <marragabriel@itis-molinari.eu>
2025-04-06 11:19:45 +0200

    esercizio guidato

--- server.js
+++ server.js
@@ -17,11 +17,16 @@ app.use(
 app.use("/", express.static(path.join(__dirname, "public")));
 const server = http.createServer(app);
 const io = new Server(server);
-server.listen(conf.port, () => {
-  console.log("server running on port: " + conf.port);
-
+io.on('connection', (socket) => {
+   console.log("socket connected: " + socket.id);
+   io.emit("chat", "new client: " + socket.id);
+   socket.on('message', (message) => {
+      const response = socket.id + ': ' + message;
+      console.log(response);
+      io.emit("chat", response);
+   });
 });
 
-io.on('connection', (socket) => {
-    console.log("socket connected: " + socket.id);
+server.listen(conf.port, () => {
+    console.log("server running on port: " + conf.port);  
 });
